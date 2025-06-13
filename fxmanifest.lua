665caedce876f456d492a080787f63358324ab59
J4NIEL <70277881+J4NIEL@users.noreply.github.com>
2025-06-13 03:22:08 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Pearls Job'
-description 'Fishing restaurant job for qb-core'
+author 'JANIEL'
+description 'Pearls restaurant job for qb-core'
 
 shared_script 'shared.lua'
 client_script 'client.lua'
